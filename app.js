cfc12705f5c8f7b9487ae814ba785304846b3f13
Hamza Mustafa { Alginis } <hamza.m.gaming@outlook.com>
2022-11-20 01:43:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const port = 	20.119.144.4;
+const port = 	8080;
 //const helmet = require("helmet");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
